3df8cbf23143941520c2dcb82d5bcf402361f83b
jhsung-PC\jhsung <jhsung@hubilon.co.kr>
2017-03-22 17:32:36 +0900

    Filter a null value from a Stream (filter(Objects::nonNull))

---
+++ src/example/stream_example/FilterNullValue.java
@@ -0,0 +1,34 @@
+package example.stream_example;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import example.util.PrintUtil;
+
+public class FilterNullValue {
+
+	public static void main(String[] args) {
+		Supplier<Stream<String>> streamSupplier = () -> Stream.of("java", "python", "node", null, "ruby", null, "php");
+		
+		// non filtering null value
+		List<String> nonFiltering = streamSupplier.get()
+				.collect(Collectors.toList());
+		PrintUtil.printList("non filtering null value", nonFiltering);
+		
+		// filtering null value
+		List<String> filtering = streamSupplier.get()
+				.filter(i -> i!=null)
+				.collect(Collectors.toList());
+		PrintUtil.printList("filtering null value", filtering);
+		
+		// Alternatively, filter with Objects::nonNull
+		List<String> result = streamSupplier.get()
+				.filter(Objects::nonNull)
+				.collect(Collectors.toList());
+		PrintUtil.printList("filter with Objects::nonNull", result);
+	}
+
+}
